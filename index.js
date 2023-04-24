63f82e52b370ee9b673d37c527720c61217fee85
windman1111 <123778061+windman1111@users.noreply.github.com>
2023-04-25 00:05:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const fecha_nacimiento = new Date("september 21 1980");
+const lista = ["Andrés", 42, true, fecha_nacimiento]
+console.log(lista)
+
+libro = {
+    titulo: "Los pilares de la tierra",
+    autor: "Ken Follet",
+    fecha: 1992,
+    url: "https://www.lospilaresdelatierra.com"
+}
